c214bf3f103356e2c64936cd021d152fdcc8eec6
Nikolai Perlt <nikolaiperlt@gmail.com>
2022-01-02 21:46:34 +0100

    stuff

--- copyConfigs.sh
+++ copyConfigs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cp -r ./qtile ~/.config && echo "Copied qtile configs"
-cp -r ./picom ~/.config && echo "Copied picom configs"
-cp -r ./autokey ~/.config && echo "Copied autokey configs"
-cp -r ./rofi ~/.config && echo "Copied rofi configs"
-cp -r ./dunst ~/.config && echo "Copied dunst configs"
+rm -r ~/.config/qtile && cp -r ./qtile ~/.config && echo "Copied qtile configs"
+rm -r ~/.config/picom && cp -r ./picom ~/.config && echo "Copied picom configs"
+rm -r ~/.config/autokey && cp -r ./autokey ~/.config && echo "Copied autokey configs"
+rm -r ~/.config/rofi && cp -r ./rofi ~/.config && echo "Copied rofi configs"
+rm -r ~/.config/dunst && cp -r ./dunst ~/.config && echo "Copied dunst configs"
 
 cp ./bash/.bashrc ~/.bashrc && echo "Copied bashrc configs"
 cp ./bash/.shell_aliases ~/.shell_aliases && echo "Copied shell_aliases configs"
